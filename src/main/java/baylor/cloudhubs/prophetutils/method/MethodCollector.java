90dfbdbd0ae05e02d262b745bac7cf77f5b78918
Adam Kattan <kattanadam7@gmail.com>
2024-10-20 17:40:50 +0200

    feat: using also msName for methods

--- src/main/java/baylor/cloudhubs/prophetutils/method/MethodCollector.java
+++ src/main/java/baylor/cloudhubs/prophetutils/method/MethodCollector.java
@@ -8,14 +8,14 @@ import java.io.*;
 
 public class MethodCollector {
 
-    private static final JsonArray jsonArray = new JsonArray();
+    private static final JsonArray msArray = new JsonArray();
 
     public static void dump(File result) {
         try (var fw = new FileWriter(result)) {
             GsonBuilder gsonBuilder = new GsonBuilder();
             gsonBuilder.setPrettyPrinting();
             JsonObject jsonObject = new JsonObject();
-            jsonObject.add("methods", jsonArray);
+            jsonObject.add("methods", msArray);
             fw.write(gsonBuilder.create().toJson(jsonObject));
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -25,14 +25,20 @@ public class MethodCollector {
     public static void parseMethodsCsv(File csvFile) {
         try (var br = new BufferedReader(new FileReader(csvFile))) {
             String line;
+            String msName = csvFile.getName().substring(0, csvFile.getName().indexOf("_methods.csv"));
+            JsonObject ms = new JsonObject();
+            JsonArray methods = new JsonArray();
+            ms.addProperty("name", msName);
+
             while ((line = br.readLine()) != null) {
                 String[] cols = line.split(",");
                 JsonObject jsonObject = new JsonObject();
                 jsonObject.addProperty("name", cols[0]);
                 jsonObject.addProperty("bytecodeHash", cols[1]);
-
-                jsonArray.add(jsonObject);
+                methods.add(jsonObject);
             }
+            ms.add("methods", methods);
+            msArray.add(ms);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
